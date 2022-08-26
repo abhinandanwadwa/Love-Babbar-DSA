fe7b332ea127dc3e9fb717b659cf205ef028f46d
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-26 17:13:36 +0530

    Different Types of Variables in C++

--- Lecture 2/NamasteDunia.cpp
+++ Lecture 2/NamasteDunia.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
 int main() {
-    cout << "Namaste Dunia :-)" << endl;    
+  int a = 123;
+  cout << a << endl;
+
+  char b = 'v';
+  cout << b << endl;
+
+  bool bl = true;
+  cout << bl << endl;
+
+  float f = 1.2;
+  cout << f << endl;
+
+  double d = 1.23;
+  cout << d << endl;
 }
