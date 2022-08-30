694b02090b67da88ccad69c31755a7f3b0eb81d1
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-30 12:26:00 +0530

    Using cin.get()

--- Lecture 3/main.cpp
+++ Lecture 3/main.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 int main() {
-    int a;
-    cin >> a;
+    // int a;
+    // cin >> a;
 
     // cout << "The Value of n is: " << n << endl;
 
     // If a is positive
-    if (a > 0) {
-        cout << "A is Positive" << endl;
-    }
-    else {
-        cout << "A is negative" << endl;
-    }
+    // if (a > 0) {
+    //     cout << "A is Positive" << endl;
+    // }
+    // else {
+    //     cout << "A is negative" << endl;
+    // }
+
+
+    int a;
+    // cin >> a >> b;
+    a = cin.get();
+
+    cout << a << endl;
 
     return 0;
 }
