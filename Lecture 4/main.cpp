258fc14d8575553f9018aa081f183bb628c4af72
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-30 23:44:30 +0530

    Pattern Q4

--- Lecture 4/main.cpp
+++ Lecture 4/main.cpp
@@ -6,16 +6,14 @@ int main() {
     cin >> n;
 
     int i = 1;
-
     while (i <= n) {
         int j = 1;
-
         while (j <= n) {
-            cout << j << " ";
+            cout << n-j+1 << " ";
             j++;
         }
-
         cout << endl;
+
         i++;
     }
 
