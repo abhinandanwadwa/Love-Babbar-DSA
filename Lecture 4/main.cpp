5a3a170508f32bed11af5dcf58a028ddbda185f6
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-31 00:19:39 +0530

    Pattern Q6 (Alternate)

--- Lecture 4/main.cpp
+++ Lecture 4/main.cpp
@@ -7,8 +7,8 @@ int main() {
 
     int i = 1;
     while (i <= n) {
-        int j = n-i+1;
-        while (j <= n) {
+        int j = 1;
+        while (j <= i) {
             cout << "* ";
             j++;
         }
