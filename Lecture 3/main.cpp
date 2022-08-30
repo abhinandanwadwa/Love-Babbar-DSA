ca208bd66a5a75fba6f0b0894d1e34b6df6c3871
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-30 12:17:38 +0530

    Simple if else

--- Lecture 3/main.cpp
+++ Lecture 3/main.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    cout << "The Value of n is: " << n << endl;
+    int a;
+    cin >> a;
+
+    // cout << "The Value of n is: " << n << endl;
+
+    // If a is positive
+    if (a > 0) {
+        cout << "A is Positive" << endl;
+    }
+    else {
+        cout << "A is negative" << endl;
+    }
 
     return 0;
 }
