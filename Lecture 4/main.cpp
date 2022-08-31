83fcd795006e8349d35ed203a3f7ae5968c16b9c
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-31 12:13:53 +0530

    Pattern Q15

--- Lecture 4/main.cpp
+++ Lecture 4/main.cpp
@@ -6,16 +6,16 @@ int main() {
     cin >> n;
 
     int row = 1;
-
     while (row <= n) {
         int col = 1;
-        while (col <= n) {
-            char ch = 64 + row + col - 1;
+        char ch = 64 + row;
+        while (col <= row) {
             cout << ch << " ";
             col++;
         }
         cout << endl;
         row++;
     }
+
     return 0;
 }
