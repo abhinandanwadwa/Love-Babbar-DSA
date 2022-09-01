89219c65ba8ef76057c3f3ba708dd506b0726082
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 00:00:05 +0530

    Pattern Q21

--- Lecture 4/main.cpp
+++ Lecture 4/main.cpp
@@ -4,17 +4,25 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
+    
     int row = 1;
     while (row <= n) {
+
+        // Print Spaces First
+        int spaces = row - 1;
+        while (spaces) {
+            cout << " ";
+            spaces--;
+        }
+
+        // Print Stars after printing the spaces
         int col = 1;
         while (col <= n-row+1) {
-            cout << "* ";
+            cout << "*";
             col++;
         }
         cout << endl;
         row++;
     }
-
     return 0;
 }
