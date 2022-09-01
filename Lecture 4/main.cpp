d21163a0aaaf2dd1176ee405eb4cf05d8a34b9c6
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 00:28:09 +0530

    Pattern Q26 (My Way YAYYYYYY)

--- Lecture 4/main.cpp
+++ Lecture 4/main.cpp
@@ -6,27 +6,36 @@ int main() {
     cin >> n;
 
     int row = 1;
-    int count = 1;
-
     while (row <= n) {
-        
-        // Print Spaces First
+
+        // First, Print the Spaces
         int spaces = n-row;
         while (spaces) {
             cout << " ";
             spaces--;
         }
-        
-        // Then, Print The Numbers
-        int col = 1;
-        while (col <= row) {
-            cout << count;
-            col++;
-            count++;
+
+        // Then, Print the first part of Numbers
+        int col1 = 1;
+        while (col1 <= row-1) {
+            cout << col1;
+            col1++;
         }
+
+        // Then, Print the second part of Numbers
+        cout << row;
+
+        // Then, Print the third part of Numbers
+        int col2 = 1;
+        while (col2 <= row-1) {
+            cout << row-col2;
+            col2++;
+        }
+
+
+
         cout << endl;
         row++;
     }
-
     return 0;
 }
