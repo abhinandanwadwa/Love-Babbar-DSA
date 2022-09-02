98611587c34bf183f724c2a2d50b8744e25668dd
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 14:41:48 +0530

    Normal "for" Loop

--- Lecture 5/main.cpp
+++ Lecture 5/main.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-    int a = 1;
-    int b = a++;
-    int c = ++a;
-    cout << b;
-    cout << c;
+    int n;
+    cin >> n;
+
+    cout << "Printing count from 1 to n..." << endl;
+
+    for(int i = 0; i<n; i++) {
+        cout << i+1 << " ";
+    }
     return 0;
 }
