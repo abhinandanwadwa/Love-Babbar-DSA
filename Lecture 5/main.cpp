84c06f1c1473d51edb54e279025f00c0f15d8b51
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 14:10:32 +0530

    Output Q3

--- Lecture 5/main.cpp
+++ Lecture 5/main.cpp
@@ -2,15 +2,7 @@
 using namespace std;
 
 int main() {
-    int a = 1;
-    int b = 2;
-
-    if (a-- > 0 && ++b > 2) {
-        cout << "Stage1 - Inside If ";
-    }
-    else {
-        cout << "Stage2 - Inside else";
-    }
-    cout << a << " " << b << endl;
+    int number = 3;
+    cout << (25 * (++number));
     return 0;
 }
