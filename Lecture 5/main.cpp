bdce51dd383501e6108e68dbb2629d656b8aa94b
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 12:49:29 +0530

    Left Shift and Right Shift Operators Basics

--- Lecture 5/main.cpp
+++ Lecture 5/main.cpp
@@ -2,13 +2,30 @@
 using namespace std;
 
 int main() {
-    int a = 4;
-    int b = 6;
+    
+    // Left Shift
+    int n = (3 << 2);  // Left Shift the number '3' two times
+    cout << n << endl;
 
-    cout << "a & b is: " << (a&b) << endl;
-    cout << "a | b is: " << (a|b) << endl;
-    cout << "~a is: " << ~a << endl;
-    cout << "a^b is: " << (a^b) << endl;
+    // IMPORTANT: 
+    // 1. If we Left Shift a number one time, it gets divided by '2' one time.
+    // 2. If we Left Shift a number two times, it gets divided by '2' two times
+    
+    // and so on.....
+
+
+
+    // Right Shift
+    int n1 = (8 >> 3); // Right Shift the number '8 ' two times
+    cout << n1 << endl;
+
+
+    // IMPORTANT: 
+    // 1. If we Right Shift a number one time, it gets multiplied by '2' one time.
+    // 2. If we Right Shift a number two times, it gets multiplied by '2' two times
+    
+    // and so on.....
+    
   
     return 0;
 }
