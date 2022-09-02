d1d094745562d194bdf570ef7f14058d81c124d4
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 14:04:41 +0530

    Output Q1

--- Lecture 5/main.cpp
+++ Lecture 5/main.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-    int i = 7;
-
-    cout << (++i) << endl;
-    cout << (i++) << endl;
-    cout << i << endl;
+    int a, b = 1;
+    a = 10;
+    if (++a) {
+        cout << b;
+    }
+    else {
+        cout << ++b;
+    }
     return 0;
 }
