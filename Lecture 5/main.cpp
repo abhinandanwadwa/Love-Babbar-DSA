6e7a6c579d4603773c108fce0adf001abe27bf14
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 12:24:02 +0530

    Bitwise Operators (Basic)

---
+++ Lecture 5/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+
+int main() {
+    int a = 4;
+    int b = 6;
+
+    cout << "a & b is: " << (a&b) << endl;
+    cout << "a | b is: " << (a|b) << endl;
+    cout << "~a is: " << ~a << endl;
+    cout << "a^b is: " << (a^b) << endl;
+  
+    return 0;
+}
