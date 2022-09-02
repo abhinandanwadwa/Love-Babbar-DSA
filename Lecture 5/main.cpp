7a1470ddb5876c0953a78a1a71255984ca1f7464
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-02 14:00:11 +0530

    Pre and Post Increment

--- Lecture 5/main.cpp
+++ Lecture 5/main.cpp
@@ -1,31 +1,11 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
 
 int main() {
-    
-    // Left Shift
-    int n = (3 << 2);  // Left Shift the number '3' two times
-    cout << n << endl;
+    int i = 7;
 
-    // IMPORTANT: 
-    // 1. If we Left Shift a number one time, it gets multiplied by '2' one time.
-    // 2. If we Left Shift a number two times, it gets multiplied by '2' two times
-    
-    // and so on.....
-
-
-
-    // Right Shift
-    int n1 = (8 >> 3); // Right Shift the number '8 ' two times
-    cout << n1 << endl;
-
-
-    // IMPORTANT: 
-    // 1. If we Right Shift a number one time, it gets divided by '2' one time.
-    // 2. If we Right Shift a number two times, it gets divided by '2' two times
-    
-    // and so on.....
-    
-  
+    cout << (++i) << endl;
+    cout << (i++) << endl;
+    cout << i << endl;
     return 0;
 }
