0c28a2bebc6468157dde26d03644cb4f08d93456
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-10 12:38:16 +0530

    Making a function to print the elements inside an array

--- Lecture 9/array.cpp
+++ Lecture 9/array.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";   
+}
+
 int main() {
     int third[15] = {2, 7};
 
-    for (int i = 0; i < 15; i++)
-    {
-        cout << third[i] << endl;
-    }
+    printArray(third, 15);
     
     return 0;
 }
