71587ee567118c7774e973e7ff2f67c6916f0751
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-11 09:01:35 +0530

    Homework 1: Printing sum of all elements of an array

---
+++ Lecture 9/SumOfArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << sum << endl;
+    
+    return 0;
+}
