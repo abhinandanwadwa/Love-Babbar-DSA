480cf5a476f5d72cd1cb36e66e048c796c069d0a
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-27 15:23:09 +0530

    Bubble Sort Alternate Optimized

--- Lecture 17/BubbleSortAlt.cpp
+++ Lecture 17/BubbleSortAlt.cpp
@@ -3,11 +3,16 @@ void bubbleSort(vector<int>& arr, int n)
 {   
     for (int i=1; i<n; i++) {
         // for round 1 to n-1
+        bool swapped = false;  // The optimization
         for (int j=0; j<n-i; j++) {
-            // process element till (n-1)th index
+            // p rocess element till (n-1)th index
             if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
+                swapped = true;  // The optimization
             }
+        }
+        if (swapped == false) {
+            break;
         }
     }
 }
