3aa09c7e9a10786a44267b25bb6e7a262c76bf2e
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-27 15:10:25 +0530

    Bubble Sort Alternate Method

---
+++ Lecture 17/BubbleSortAlt.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for (int i=1; i<n; i++) {
+        // for round 1 to n-1
+        for (int j=0; j<n-i; j++) {
+            // process element till (n-1)th index
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
