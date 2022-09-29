9b37a1b3d0d80800b3f309c8b5ac1929ef9efe17
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-29 15:26:51 +0530

    Stack in STL

---
+++ Lecture 19/StackSTL.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    stack<string> s;
+
+    s.push("Love");
+    s.push("Babbar");
+    s.push("Kumar");
+
+    cout << "Top Element -> " << s.top() << endl;
+
+    s.pop();
+    cout << "Top Element -> " << s.top() << endl;
+
+    cout << "Size of Stack (After Pop) -> " << s.size() << endl;
+
+    cout << "Empty or not -> " << s.empty() << endl;
+
+    return 0;
+}
