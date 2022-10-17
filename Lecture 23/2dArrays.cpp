3a45da0f36fb39d5a3cbe02b9ff4f9ad4a569e81
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-17 14:16:07 +0530

    Basic 2d Arrays Input Output

---
+++ Lecture 23/2dArrays.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // Creating a 2d Array
+    int arr[3][4];
+
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+
+    // Priting the 2d Array
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
